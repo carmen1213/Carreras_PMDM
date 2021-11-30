71726d756725dfdc96fdc6070dc7666ed741277c
Carmen <cmarti@campuscamarafp.com>
2021-11-30 14:02:07 +0100

    tresactivity

---
+++ app/src/main/java/com/example/carreras/Coches.kt
@@ -0,0 +1,11 @@
+package com.example.carreras
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Coches : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coches)
+    }
+}
---
+++ app/src/main/java/com/example/carreras/coche_jugar.kt
@@ -0,0 +1,11 @@
+package com.example.carreras
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class coche_jugar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coche_jugar)
+    }
+}
